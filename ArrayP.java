32b2189b5e00a93958b0a07ea414c945df0a3717
sumitranjanDroider <desidroider@gmail.com>
2023-08-17 16:21:00 +0530

    Chenged message and not array will show value with index

--- ArrayP.java
+++ ArrayP.java
@@ -3,7 +3,7 @@ class ArrayP{
   public static void main(String[] args){
    int arr[]={1,2,3,4,5,6,7,8,9};
     for(int i=0;i<arr.length;i++){
-        System.out.println(arr[i]);
+        System.out.println("Index "+i+" Value Present "+arr[i]);
      }
 //For each
     for(int value:arr){
