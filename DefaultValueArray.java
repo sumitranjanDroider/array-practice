a4a0fd06b050d1fef7ec29dc931eff63050e91f2
sumitranjanDroider <desidroider@gmail.com>
2023-08-21 11:33:25 +0530

    Array default values practice calss

---
+++ DefaultValueArray.java
@@ -0,0 +1,10 @@
+class DefaultValueArray{
+
+  public static void main(String[] ar){
+   
+     System.out.println("String array default values:");
+     String str[]=new String[5];
+     for(String s:str)
+     System.out.println(s+" ");
+}
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Practicing On Array 
+1.DefaultValueArray.class
+ ->This class i made for practicing about topic default array value
+   whe we have not added any value in array what value array holds.
